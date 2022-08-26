13bfae88c7fa9e1aaaf5e4c316810b8ca8046077
Kúp <112136726+Khiyam@users.noreply.github.com>
2022-08-26 12:52:47 +0400

    Update bootra1n.sh

--- bootra1n.sh
+++ bootra1n.sh
@@ -6,4 +6,3 @@
 wget https://assets.checkra.in/downloads/linux/cli/x86_64/dac9968939ea6e6bfbdedeb41d7e2579c4711dc2c5083f91dced66ca397dc51d/checkra1n
 chmod +x checkra1n
 sudo mv checkra1n /usr/bin/
-sudo checkra1n
