f94ef6c197d3b4cbb4c6e2cc4979984e05078560
Kúp <112136726+Khiyam@users.noreply.github.com>
2022-08-26 04:10:33 -0400

    Create bootra1n.sh

---
+++ bootra1n.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# github.com/https://github.com/gyke69/bootra1n
+# Copyright (c) 2019 Anosh Khiyam
+
+wget https://assets.checkra.in/downloads/linux/cli/x86_64/dac9968939ea6e6bfbdedeb41d7e2579c4711dc2c5083f91dced66ca397dc51d/checkra1n
+chmod +x checkra1n
+sudo mv checkra1n /usr/bin/
+checkra1n
